3ba32484946d669c351bbb8655d3e90445439b21
Amrit Pariyar <amrit.pariyar1@gmail.com>
2020-02-26 23:34:03 +0545

    "function test"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'game#play'
   get 'randomchar', to: 'game#randomchar'
+  get 'game/play'
   # get 'game/play'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/game_controller_test.rb
+++ test/controllers/game_controller_test.rb
@@ -6,4 +6,12 @@ class GameControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should generate 16 size array of characters" do
+    get randomchar_url, xhr: true
+    @charset = Array('A'...'Z')
+    @chararray=Array.new(16){@charset.sample}
+    assert_equal @chararray.length, 16
+    # assert_not_includes @charset, @chararray, @response.body
+    assert_equal "application/json", @response.media_type
+  end
 end
